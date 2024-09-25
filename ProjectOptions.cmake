65ee78358a180180d8eb2838529f4f69c3c8bffc
Template Janitor <template.janitor@example.com>
2024-09-25 04:20:04 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -4,7 +4,7 @@ include(CMakeDependentOption)
 include(CheckCXXCompilerFlag)
 
 
-macro(myproject_supports_sanitizers)
+macro(vipix_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
     set(SUPPORTS_UBSAN ON)
   else()
@@ -18,183 +18,183 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(vipix_setup_options)
+  option(vipix_ENABLE_HARDENING "Enable hardening" ON)
+  option(vipix_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    vipix_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    vipix_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  vipix_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR vipix_PACKAGING_MAINTAINER_MODE)
+    option(vipix_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(vipix_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(vipix_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(vipix_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(vipix_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(vipix_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(vipix_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(vipix_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(vipix_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(vipix_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(vipix_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(vipix_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(vipix_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(vipix_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(vipix_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(vipix_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(vipix_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(vipix_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(vipix_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(vipix_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(vipix_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(vipix_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(vipix_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(vipix_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(vipix_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(vipix_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      vipix_ENABLE_IPO
+      vipix_WARNINGS_AS_ERRORS
+      vipix_ENABLE_USER_LINKER
+      vipix_ENABLE_SANITIZER_ADDRESS
+      vipix_ENABLE_SANITIZER_LEAK
+      vipix_ENABLE_SANITIZER_UNDEFINED
+      vipix_ENABLE_SANITIZER_THREAD
+      vipix_ENABLE_SANITIZER_MEMORY
+      vipix_ENABLE_UNITY_BUILD
+      vipix_ENABLE_CLANG_TIDY
+      vipix_ENABLE_CPPCHECK
+      vipix_ENABLE_COVERAGE
+      vipix_ENABLE_PCH
+      vipix_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  vipix_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (vipix_ENABLE_SANITIZER_ADDRESS OR vipix_ENABLE_SANITIZER_THREAD OR vipix_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(vipix_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(vipix_global_options)
+  if(vipix_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    vipix_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  vipix_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(vipix_ENABLE_HARDENING AND vipix_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR vipix_ENABLE_SANITIZER_UNDEFINED
+       OR vipix_ENABLE_SANITIZER_ADDRESS
+       OR vipix_ENABLE_SANITIZER_THREAD
+       OR vipix_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${vipix_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${vipix_ENABLE_SANITIZER_UNDEFINED}")
+    vipix_enable_hardening(vipix_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(vipix_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(vipix_warnings INTERFACE)
+  add_library(vipix_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  vipix_set_project_warnings(
+    vipix_warnings
+    ${vipix_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
     "")
 
-  if(myproject_ENABLE_USER_LINKER)
+  if(vipix_ENABLE_USER_LINKER)
     include(cmake/Linker.cmake)
-    myproject_configure_linker(myproject_options)
+    vipix_configure_linker(vipix_options)
   endif()
 
   include(cmake/Sanitizers.cmake)
-  myproject_enable_sanitizers(
-    myproject_options
-    ${myproject_ENABLE_SANITIZER_ADDRESS}
-    ${myproject_ENABLE_SANITIZER_LEAK}
-    ${myproject_ENABLE_SANITIZER_UNDEFINED}
-    ${myproject_ENABLE_SANITIZER_THREAD}
-    ${myproject_ENABLE_SANITIZER_MEMORY})
+  vipix_enable_sanitizers(
+    vipix_options
+    ${vipix_ENABLE_SANITIZER_ADDRESS}
+    ${vipix_ENABLE_SANITIZER_LEAK}
+    ${vipix_ENABLE_SANITIZER_UNDEFINED}
+    ${vipix_ENABLE_SANITIZER_THREAD}
+    ${vipix_ENABLE_SANITIZER_MEMORY})
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(vipix_options PROPERTIES UNITY_BUILD ${vipix_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(vipix_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      vipix_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(vipix_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    vipix_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(vipix_ENABLE_CLANG_TIDY)
+    vipix_enable_clang_tidy(vipix_options ${vipix_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(vipix_ENABLE_CPPCHECK)
+    vipix_enable_cppcheck(${vipix_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(vipix_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    vipix_enable_coverage(vipix_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(vipix_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(vipix_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(vipix_ENABLE_HARDENING AND NOT vipix_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR vipix_ENABLE_SANITIZER_UNDEFINED
+       OR vipix_ENABLE_SANITIZER_ADDRESS
+       OR vipix_ENABLE_SANITIZER_THREAD
+       OR vipix_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    vipix_enable_hardening(vipix_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
--- src/sample_library/sample_library.cpp
+++ src/sample_library/sample_library.cpp
@@ -1,4 +1,4 @@
-#include <myproject/sample_library.hpp>
+#include <vipix/sample_library.hpp>
 
 int factorial(int input) noexcept
 {
